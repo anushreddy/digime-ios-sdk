f8ab57184b391bb2a0b983e81f4de3171ba5ff52
Oli <oliver@digi.me>
2017-10-07 11:20:17 +0100

    Update Constants.swift

--- Example/DigiMeFramework/Constants.swift
+++ Example/DigiMeFramework/Constants.swift
@@ -8,8 +8,8 @@
 
 struct staticConstants {
     static let kAppID               = "skeleton"
-    static let kContractID1         = "your_real_contract_id" // a contract for two years' content
-    static let kContractID2         = "your_real_contract_id" // a contract for three months' content
+    static let kContractID1         = "your_real_contract_id" // prod-2y contract-id
+    static let kContractID2         = "your_real_contract_id" // prod-3m contract-id
     static let kP12FileName1        = "CA_RSA_PRIVATE_KEY1"
     static let kP12FileName2        = "CA_RSA_PRIVATE_KEY2"
     static let kP12Password         = "digime"
